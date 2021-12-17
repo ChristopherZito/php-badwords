febf561427af3b20bd9d2a5970f56014ae9d12b9
christopherzito <christopherzito11@gmail.com>
2021-12-17 14:26:39 +0100

    prova di upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    
+</head>
+<body>
+
+<?php 
+
+    $x = 5;
+    $y = 10;
+    $z = $x + $y;
+?>
+    <h1>
+        somma di <?php echo $x ." e di " . $y . " uguale a " . $z?>
+    </h1>
+</body>
+</html>
